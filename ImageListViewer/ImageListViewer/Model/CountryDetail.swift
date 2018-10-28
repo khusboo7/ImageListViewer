b9cfa8a290b06ae997bcd320844d66f5ab3f5e81
khusboo <>
2018-10-28 23:04:24 +0530

    created model codable struct to decode the json response

--- ImageListViewer/ImageListViewer/Model/CountryDetail.swift
+++ ImageListViewer/ImageListViewer/Model/CountryDetail.swift
@@ -2,8 +2,19 @@
 //  CountryDetail.swift
 //  ImageListViewer
 //
-//  Created by Techjini on 27/10/18.
+//  Created by khusboo on 27/10/18.
 //  Copyright © 2018 Khusboo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct CountryDetail : Codable{
+    let title : String?
+    let description : String?
+    let imageHref :String?
+}
+
+struct Country : Codable{
+    let title : String
+    let rows : [CountryDetail]?
+}
